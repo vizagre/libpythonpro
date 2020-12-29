02fdd75069be56f7a7b31d07113a113a677e2d78
Leonardo Vizagre <leonardo.vizagre@gmail.com>
2020-12-29 18:45:08 -0300

    Implementar a lib MOCK no projeto

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.spam.enviador_de_email import Enviador
@@ -20,39 +22,27 @@ from libpythonpro.spam.modelos import Usuario
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_span = EnviadorDeSpam(sessao, enviador)
     enviador_de_span.enviar_emails(
         'leonardo.vizagre@gmail.com',
         'Curso Python Pro',
         'Módulo PyTools'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
-
-
-class EnviadorMock(Enviador):
-
-    def __init__(self):
-        super().__init__()
-        self.qtd_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_email_enviados += 1
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Vizagre', email='kimi.vizagre@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_span = EnviadorDeSpam(sessao, enviador)
     enviador_de_span.enviar_emails(
         'amanda.vizagre@gmail.com',
         'Curso Python Pro',
         'Módulo PyTools'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with == (
         'amanda.vizagre@gmail.com',
         'kimi.vizagre@gmail.com',
         'Curso Python Pro',
