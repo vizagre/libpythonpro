822b716d285e776ebc1664d23ab5c1fc050f3ef8
Leonardo Vizagre <leonardo.vizagre@gmail.com>
2020-12-29 17:04:29 -0300

    Alteração das classes para ser possível testar envio de spam com qtd variavel de usuarios

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_span = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Vizagre', email='kimi.vizagre@gmail.com'),
+            Usuario(nome='Amanda', email='kimi.vizagre@gmail.com')
+        ],
+        [
+            Usuario(nome='Vizagre', email='kimi.vizagre@gmail.com')
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_span = EnviadorDeSpam(sessao, enviador)
     enviador_de_span.enviar_emails(
         'leonardo.vizagre@gmail.com',
         'Curso Python Pro',
         'Módulo PyTools'
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,14 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Kimi')
+    usuario = Usuario(nome='Kimi', email='kimi.vizagre@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Vizagre'), Usuario(nome='Amanda')]
+    usuarios = [Usuario(nome='Vizagre', email='kimi.vizagre@gmail.com'),
+                Usuario(nome='Amanda', email='kimi.vizagre@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
